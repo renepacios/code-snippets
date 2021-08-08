c09ed865c389ce1d832dd6dc5dab3f758659e303
René Pacios <3912951+renepacios@users.noreply.github.com>
2021-08-08 14:08:04 +0200

    Help to battery status

--- ps/Battery-State.ps1
+++ ps/Battery-State.ps1
@@ -1,4 +1,8 @@
-# https://www.jasoft.org/Blog/post/como-saber-si-la-bateria-de-tu-portatil-ha-perdido-capacidad <-- 05-2021 
+# Genera un informe en c:\report del estado de salud de la bateria, ciclos de carga, etc..
+
+# refs: 
+#    - https://www.jasoft.org/Blog/post/como-saber-si-la-bateria-de-tu-portatil-ha-perdido-capacidad <-- 05-2021 
+
 
 c:
 md c:\report
